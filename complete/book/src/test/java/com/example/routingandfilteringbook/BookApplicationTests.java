6bc62dea301a2e76d460c71468a202cd9246ca69
Jay Bryant <jbryant@pivotal.io>
2019-10-24 15:06:33 -0500

    Upgraded to Spring Boot 2.2
    I upgraded the build files to Spring Boot 2.2.0, which necessitated changing the tests to work with JUnit 5. I also found that the Maven wrappers were missing, so I added them.

--- complete/book/src/test/java/com/example/routingandfilteringbook/BookApplicationTests.java
+++ complete/book/src/test/java/com/example/routingandfilteringbook/BookApplicationTests.java
@@ -1,15 +1,13 @@
 package com.example.routingandfilteringbook;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BookApplicationTests {
 
